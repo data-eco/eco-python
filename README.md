c34c91951e5c12aaa22bdb4a0828e6e64686965f
Keith Hughitt <keith.hughitt@gmail.com>
2022-01-08 20:22:13 -0700

    Isolating io/data-dag metadata into a single attribute

--- README.md
+++ README.md
@@ -1,4 +1,4 @@
-Io Python
+io-python
 =========
 
 **Status (Jan 2022)**: Planning / experimental.
@@ -6,14 +6,31 @@ Io Python
 Overview
 --------
 
-Tools for creating and interacting with [Frictionless Data
-Packages](https://frictionlessdata.io/) which including a Directed Acyclic Graph (DAG)
+_io-dag ~ metadata for data workflows_
+
+`io-python` is a python library for creating and interacting with [Frictionless Data
+Packages](https://frictionlessdata.io/) which include a Directed Acyclic Graph (DAG)
 representation of data provenance, along with optional annotations and views for each
 stage in processing.
 
+The goal is to provide a means by which users can transparently document all of the
+reasoning, assumptions, insights and questions that are typically involved in any data
+analysis, in a format which can be easily stored in version control and iteratively
+improved upon by the community of users who care about the data and would like to gain a
+deeper understanding of it.
+
+The provenance DAG, along with associated annotations and visualizations, can then be
+rendered in various endpoints such as a CLI tool or Web UI, with additional information
+about the data at each stage of processing (shape, % missing or imputed values, etc.)
+can be overlaid on the DAG, providing useful visual context to data users.
+
+With all of this, a major goal is to try and convey _uncertainty_ as transparently as
+possible.
+
 _The Basic Idea_:
 
-1. Sits on top of existing pipeline codebase
+1. Users modify their pipeline to include io "hooks" to generate & update datapackages
+   for each stage of processing.
 2. Data packages with embedded DAGs written out at each stage of processing
   - Julia/Python/R libraries help with datapackage i/o & DAG operations
 3. For each stage in data processing (node in the DAG), one can optionally include one
@@ -21,7 +38,7 @@ _The Basic Idea_:
    - annotations (markdown, or any other plain-text format)
    - data plots ([vega-lite](https://vega.github.io/))
 
-The result is a collection of data packages as output, each of which has a complete
+The resulting output is a collection of _data packages_, each of which has a complete
 history of all of the operations performed on it up to the point, including annotations
 and visualizations for each stage, intended to help convey the reasoning, motivation,
 uncertainaties, etc. associated with the processing steps.
@@ -46,8 +63,8 @@ Goals
 Limitations
 -----------
 
-1. Currently focused on supporting tabular data packages
-  - long-term, if useful, would like to extend to other forms of data
+Current efforts are focused at supporting "tabular" (matrix/dataframe) data packages.
+Long-term, the goal is is to extend support to other forms of data.
 
 TODO
 ----
@@ -59,5 +76,3 @@ TODO
 - [ ] describe motivation & use of profiles
 - [ ] allow arbitrary schemas to be provided & used for validation?
   - [ ] modify "profile" to accept a path/url/dict?
-
-
--- iodat/packager.py
+++ iodat/packager.py
@@ -24,6 +24,119 @@ class Packager:
         self._schema_dir = os.path.abspath(resource_filename(__name__, "profiles"))
         self._version = __version__
 
+    def build_package(self, resources, 
+                      annotations=List[str | pathlib.Path], 
+                      views=List[str | pathlib.Path | Dict[str, Any]], 
+                      metadata=str | pathlib.Path | Dict[str, Any], 
+                      profile="",
+                      pkg_dir="./", 
+                      include_summary=False,
+                      pretty_print=True):
+        """
+        Builds an Io datapackage.
+
+        Parameters
+        ----------
+        resources: dict
+            Dictionary of names resources to include in the data package
+        annotations: list
+            List of filepaths to annotation files, or string annotations
+        views: list
+            List of filepaths to vega-lite views, or dict representations of such views
+        metadata: str|path|dict
+            [Optional] Additional metadata to include, either as a dict, or path to a yml/json file
+        profile: str
+            [Optional] Name of metadata profile to use for validation, or "", for none
+        pkg_dir: str|path
+            [Optional] Location where data package should be saved (default: "./")
+        include_summary: bool
+            [Optional] Whether or not to compute & embed summary statistics at the time of package
+            creation (default: False)
+        """
+        # parse user-provided metadata
+        general_mdata = self._parse_metadata(metadata)
+
+        # if a metadata profile was specified, validate metadata against its schema
+        if profile != "":
+            self._validate_metadata(general_mdata, profile)
+
+        # determine location to build packages
+        pkg_dir = os.path.realpath(os.path.expanduser(os.path.expandvars(pkg_dir)))
+
+        # write resources to output dir
+        # for now, resources are first written out, and then "describe_package()" is
+        # used to infer the appropriate datapackage metadata;
+        for resource_name in resources:
+            outfile = os.path.join(pkg_dir, resource_name + ".csv")
+            resources[resource_name].to_csv(outfile, index=False)
+
+        # scan package dir to construct initial datapackage
+        pkg = frictionless.describe_package("*.csv", basepath=pkg_dir)
+
+        # ideally, it would be preferable to build things up using the already-loaded
+        # dataframes, but i haven't found a way to do this in a satisfactory manner,
+        # yet.. partial logic for such an approach below: (kh jan 8, 2022)
+        #pkg = frictionless.Package()
+        #for resource_name in resources:
+            # resource = frictionless.describe(resources[resource_name])
+            # resource.write(outfile)
+            # res = pkg.add_resource(resource)
+
+        # generate a UUID to use for the dataset at this stage in processing
+        node_uuid = str(uuid.uuid4())
+
+        now = datetime.datetime.utcnow().strftime('%Y-%m-%dT%H:%M:%S.%f%z')
+
+        node_mdata = {
+            "name": "io-python",
+            "action": "build_package",
+            "time":  now,
+            "version": self._version,
+            "annot": [],
+            "views": []
+        }
+
+        # add annotations and views, if present
+        for annot in annotations:
+            node_mdata["annot"].append(self.parse_annotation(annot))
+
+        for view in views:
+            node_mdata["views"].append(self.parse_view(view))
+
+        # add io-dag section to data package metadata
+        pkg["io-dag"] = {
+            "metadata": general_mdata,
+            "uuid": node_uuid,
+            "nodes": { 
+                node_uuid: node_mdata
+            },
+            "edges": {}
+        }
+
+        # add num rows/columns to metadata;
+        # this may eventually be moved to the optional "compute_summary" section
+        #  for i, resource in enumerate(pkg["resources"]):
+        #      dims = resources[resource['name']].shape
+        #
+        #      pkg["resources"][i]["num_rows"] = dims[0]
+        #      pkg["resources"][i]["num_columns"] = dims[1]
+
+        # add summary statistics
+        # todo: allow summary stats to be enabled at the resource-level?
+        #  if include_summary:
+        #      try:
+        #          data_path = os.path.join(pkg_dir, "data.tsv")
+        #          pkg["summary"] = compute_summary(data_path)
+        #      except:
+        #          raise RuntimeError("Error computing dataset summary")
+
+        # write metadata to disk
+        with open(os.path.join(pkg_dir, "datapackage.json"), "w") as fp:
+            if pretty_print:
+                json.dump(pkg, fp, indent=2, sort_keys=True)
+            else:
+                json.dump(pkg, fp)
+
     def _parse_metadata(self, mdata: str | pathlib.Path | Dict[str, Any]) -> Dict[str, Any]:
         """
         Parses metadata and validates it against a specific profile, if specified
@@ -97,120 +210,6 @@ class Packager:
             print(f"mdata validation failed!")
             print(validator.errors)
             sys.exit(1)
-
-    def build_package(self, resources, 
-                      annotations=List[str | pathlib.Path], 
-                      views=List[str | pathlib.Path | Dict[str, Any]], 
-                      metadata=str | pathlib.Path | Dict[str, Any], 
-                      profile="",
-                      pkg_dir="./", 
-                      include_summary=False,
-                      pretty_print=True):
-        """
-        Builds an Io datapackage.
-
-        Parameters
-        ----------
-        resources: dict
-            Dictionary of names resources to include in the data package
-        annotations: list
-            List of filepaths to annotation files, or string annotations
-        views: list
-            List of filepaths to vega-lite views, or dict representations of such views
-        metadata: str|path|dict
-            [Optional] Additional metadata to include, either as a dict, or path to a yml/json file
-        profile: str
-            [Optional] Name of metadata profile to use for validation, or "", for none
-        pkg_dir: str|path
-            [Optional] Location where data package should be saved (default: "./")
-        include_summary: bool
-            [Optional] Whether or not to compute & embed summary statistics at the time of package
-            creation (default: False)
-        """
-        # load metadata
-        mdata = self._parse_metadata(metadata)
-
-        # if a metadata profile was specified, validate metadata against its schema
-        if profile != "":
-            self._validate_metadata(mdata, profile)
-
-        # determine location to build packages
-        pkg_dir = os.path.realpath(os.path.expanduser(os.path.expandvars(pkg_dir)))
-
-        # write resources to output dir
-        # for now, resources are first written out, and then "describe_package()" is
-        # used to infer the appropriate datapackage metadata;
-        for resource_name in resources:
-            outfile = os.path.join(pkg_dir, resource_name + ".csv")
-            resources[resource_name].to_csv(outfile, index=False)
-
-        # scan package dir to construct initial datapackage
-        pkg = frictionless.describe_package("*.csv", basepath=pkg_dir)
-
-        # ideally, it would be preferable to build things up using the already-loaded
-        # dataframes, but i haven't found a way to do this in a satisfactory manner,
-        # yet.. partial logic for such an approach below: (kh jan 8, 2022)
-        #pkg = frictionless.Package()
-        #for resource_name in resources:
-            # resource = frictionless.describe(resources[resource_name])
-            # resource.write(outfile)
-            # res = pkg.add_resource(resource)
-
-        # generate a UUID to use for the dataset at this stage in processing
-        node_uuid = str(uuid.uuid4())
-
-        now = datetime.datetime.utcnow().strftime('%Y-%m-%dT%H:%M:%S.%f%z')
-
-        mdata['uuid'] = node_uuid 
-
-        node_mdata = {
-            "name": "io-python",
-            "action": "build_package",
-            "time":  now,
-            "version": self._version,
-            "annot": [],
-            "views": []
-        }
-
-        # add annotations and views, if present
-        for annot in annotations:
-            node_mdata["annot"].append(self.parse_annotation(annot))
-
-        for view in views:
-            node_mdata["views"].append(self.parse_view(view))
-
-        # add dag to metadata
-        mdata['dag'] = {
-            "nodes": { node_uuid: node_mdata },
-            "edges": {}
-        }
-
-        # add "io" metadata section, including dag
-        pkg["io"] = mdata
-
-        # add num rows/columns to metadata;
-        # this may eventually be moved to the optional "compute_summary" section
-        #  for i, resource in enumerate(pkg["resources"]):
-        #      dims = resources[resource['name']].shape
-        #
-        #      pkg["resources"][i]["num_rows"] = dims[0]
-        #      pkg["resources"][i]["num_columns"] = dims[1]
-
-        # add summary statistics
-        # todo: allow summary stats to be enabled at the resource-level?
-        #  if include_summary:
-        #      try:
-        #          data_path = os.path.join(pkg_dir, "data.tsv")
-        #          pkg["summary"] = compute_summary(data_path)
-        #      except:
-        #          raise RuntimeError("Error computing dataset summary")
-
-        # write metadata to disk
-        with open(os.path.join(pkg_dir, "datapackage.json"), "w") as fp:
-            if pretty_print:
-                json.dump(pkg, fp, indent=2, sort_keys=True)
-            else:
-                json.dump(pkg, fp)
 
     def parse_annotation(self, annot: str | pathlib.Path):
         """
